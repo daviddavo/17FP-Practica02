19dd7e344a3d9f052db8450a30e34f101476d57d
David davo <david@ddavo.me>
2017-12-28 18:31:09 +0100

    Añadido pedirCodigo

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <cstdlib>
+#include <cstdlib> // Para random
+#include <string> // Para pedir datos al usuario
 
 using namespace std;
 
@@ -22,7 +23,7 @@ char color2char(tColor color){
 	return c;
 }
 
-tColor color2char(char c){
+tColor char2color(char c){
 	tColor color;
 	c = toupper(c); // Así siempre es mayúsculas
 
@@ -33,16 +34,54 @@ tColor color2char(char c){
 		case 'A': color = AMARILLO; break;
 		case 'M': color = MARRON; break;
 		case 'B': color = BLANCO; break;
+		default: color = INCORRECTO; break;
 	}
 
 	return color;
 }
 
+void pedirCodigo(tCodigo codigo){
+	// Primero mostramos la frase
+	cout << "Introduce el codigo (palabra de " << TAM_CODIGO << " letras con alguna de ";
+	for (unsigned int i = 0; i < INCORRECTO-1; i++){
+		cout << color2char(static_cast<tColor>(i)) << ", ";
+	}
+	cout << color2char(static_cast<tColor>(INCORRECTO-1)) << "): ";
+
+	// Y ahora pedimos el codigo, a prueba de errores
+	string line;
+	tColor color;
+
+	// cin.ignore(); // Por si acaso se ha quedado algo en el buffer // Revisar: Por alguna razon se salta el primer caracter
+	getline(cin, line);
+	if (line.length() != TAM_CODIGO){
+		cout << "El código debe ser de " << TAM_CODIGO << " letras" << endl;
+		pedirCodigo(codigo);
+	}else{
+		for (unsigned int i = 0; i < line.length(); i++){
+			color = char2color(line[i]);
+			if(color == INCORRECTO){
+				cout << "El color " << line[i] << " no existe" << endl;
+				pedirCodigo(codigo);
+				break;
+			}
+			codigo[i] = color;
+		}
+	}
+}
+
 int main(){
 	srand(time(NULL));
 
-	cout << "Hello World!" << endl;
-	cout << color2char('r') << endl;
+	tCodigo codigo;
+
+	pedirCodigo(codigo);
 
 	return 0;
 }
+
+/*	BIBLIOGRAFIA:
+ * - http://mathworld.wolfram.com/Mastermind.html
+ * - https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3022521/
+ * - http://colorcode.laebisch.com/links/Donald.E.Knuth.pdf
+ */
